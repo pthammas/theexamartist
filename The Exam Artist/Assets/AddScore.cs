9be373627f4537bcd9ed083f088c5f378d63adbf
Arpit Parwal <aparwal@usc.edu>
2020-02-18 17:55:53 -0800

    Data flow between scenes

---
+++ The Exam Artist/Assets/AddScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour
+{
+    public static int gameScore = 0;
+    public Text scoreText;
+    public Text DisplayScoreText;
+
+
+    void Update() {
+        scoreText.text = gameScore.ToString();
+        if (DisplayScoreText) {
+            DisplayScoreText.text = gameScore.ToString();
+        } 
+    }
+
+    public void incrementCount() {
+        Debug.Log(scoreText);
+        gameScore += 1;
+        Debug.Log(gameScore);
+}
+  
+}
---
+++ The Exam Artist/Assets/nextScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class nextScreen : MonoBehaviour
+{
+    
+    public void nextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void previousScene()
+    {
+        Debug.Log("I am called ");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
